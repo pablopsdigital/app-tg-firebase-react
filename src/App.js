9769518be16b1cbc03fab557e3bb0f8c2ef9bda7
pablopsdigital <pablopsdigital@gmail.com>
2023-07-04 22:39:56 +0200

    feat(router): Create routing between pages.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
-function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-}
+import React from "react";
+import { Route, Routes } from "react-router";
+import { HomePage } from "./pages/HomePage/HomePage";
+import { LoginPage } from "./pages/LoginPage/LoginPage";
+import { RegisterPage } from "./pages/RegisterPage/RegisterPage";
 
-export default App;
+export const App = () => {
+  return (
+    <div className="flex h-screen text-white bg-slate-300">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Routes>
+    </div>
+  );
+};
